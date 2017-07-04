5caf40132cfe7ee9e04b13c7f95f795fc449b8ea
James Ransome <jamesransome@Jamess-MacBook-Pro.local>
2017-07-04 15:12:57 +0100

    refactored unit test for release_bike method to one liner

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,5 @@
 require 'docking_station'
 describe DockingStation do
-
-  it 'should respond to the method release_bike' do
-    expect(DockingStation.new).to respond_to(:release_bike)
-  end
+  subject {DockingStation.new}
+  it { is_expected.to respond_to(:release_bike)} 
 end
