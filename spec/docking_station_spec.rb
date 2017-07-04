dfc9ecebf72614cfc4f2840accb0c297826509f6
James Ransome <jamesransome@Jamess-MacBook-Pro.local>
2017-07-04 16:22:14 +0100

    Passed test to check for docking station returning a bike and for bike to be working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
 require 'docking_station'
 describe DockingStation do
   subject {DockingStation.new}
-  it { is_expected.to respond_to(:release_bike)} 
+  it { is_expected.to respond_to(:release_bike)}
+
+  it 'returns an instance of Bike when released_bike is called and that the bike is working' do
+    expect(subject.release_bike).to be_an_instance_of(Bike)
+    expect(Bike.new.working?).to eq(true)
+  end
+
 end
