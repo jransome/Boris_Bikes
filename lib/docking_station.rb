c52a1570a332422310cc5671d1be8d89882b923e
James Ransome <jamesransome@msn.com>
2017-07-05 12:54:26 +0100

    stations store bikes and provide access to them

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,8 +1,14 @@
 require './lib/bike'
 
 class DockingStation
+  attr_reader :docked_bikes
 
   def release_bike
     Bike.new
   end
+
+  def dock_bike bike
+    @docked_bikes = bike
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,25 @@
 require 'docking_station'
 describe DockingStation do
-  subject {DockingStation.new}
+  subject(:station) {DockingStation.new}
   it { is_expected.to respond_to(:release_bike)}
 
   it 'returns an instance of Bike when released_bike is called and that the bike is working' do
     expect(subject.release_bike).to be_an_instance_of(Bike)
     expect(Bike.new.working?).to eq(true)
+  end
+
+  it 'docks a bike at the station and shows bikes currently docked there' do
+    bike = Bike.new
+    station.dock_bike(bike)
+    expect(station.docked_bikes).to eq(bike)
+  end
+
+  it 'responds to a docking method' do
+    expect(station).to respond_to(:dock_bike).with(1).argument
+  end
+
+  it 'responds to .docked_bikes method' do
+    expect(station).to respond_to(:docked_bikes)
   end
 
 end
